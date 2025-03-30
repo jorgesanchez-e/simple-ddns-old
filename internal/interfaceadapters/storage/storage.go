8353735f4c203dc75e245e15f4668cb23020262b
Jorge Sánchez Escobedo <jorge.sanchez.escobedo@gmail.com>
2025-03-30 00:21:18 -0600

    storage now uses config

---
+++ internal/interfaceadapters/storage/storage.go
@@ -0,0 +1,46 @@
+package storage
+
+import (
+	"encoding/json"
+	"io"
+
+	"github.com/jorgesanchez-e/simple-ddns/internal/domain/ddns"
+	"github.com/jorgesanchez-e/simple-ddns/internal/interfaceadapters/storage/sqlite"
+)
+
+const (
+	storage_config      string = "app.storage"
+	sqlite_config       string = "app.storage.sqlite"
+	sqlite_storage_name string = "sqlite"
+)
+
+type ConfigReader interface {
+	Find(node string) (io.Reader, error)
+}
+
+func NewService(cnf ConfigReader) (ddns.Repository, error) {
+	storageConfigReader, err := cnf.Find(storage_config)
+	if err != nil {
+		return nil, err
+	}
+
+	storageConfig, err := io.ReadAll(storageConfigReader)
+	if err != nil {
+		return nil, err
+	}
+
+	var config map[string]interface{}
+	if err = json.Unmarshal(storageConfig, &config); err != nil {
+		return nil, err
+	}
+
+	if _, exists := config[sqlite_storage_name]; exists {
+		if storageConfigReader, err = cnf.Find(sqlite_config); err != nil {
+			return nil, err
+		}
+
+		return sqlite.New(cnf)
+	}
+
+	return nil, nil
+}
