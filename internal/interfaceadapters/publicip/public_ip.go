8895beee08690cf6f1a8615be2e9ae5a813ccb81
Jorge Sánchez Escobedo <jorge.sanchez.escobedo@gmail.com>
2025-04-13 16:35:48 -0600

    public ip refactored

--- internal/interfaceadapters/publicip/public_ip.go
+++ internal/interfaceadapters/publicip/public_ip.go
@@ -1,10 +1,10 @@
 package publicip
 
 import (
-	"context"
 	"encoding/json"
 	"io"
 
+	"github.com/jorgesanchez-e/simple-ddns/internal/domain/ddns"
 	"github.com/jorgesanchez-e/simple-ddns/internal/interfaceadapters/publicip/ipify"
 )
 
@@ -17,12 +17,7 @@ type ConfigReader interface {
 	Find(node string) (io.Reader, error)
 }
 
-type PublicIPs interface {
-	IPV4(ctx context.Context) (string, error)
-	IPV6(ctx context.Context) (string, error)
-}
-
-func NewService(cnf ConfigReader) (PublicIPs, error) {
+func NewService(cnf ConfigReader) (ddns.PublicIPGetter, error) {
 	cnfReader, err := cnf.Find(public_ip_config_path)
 	if err != nil {
 		return nil, err
